77bf4261855cf63c4c3e58314f246f045db238e2
ctstanley <ctstanley1@hotmail.com>
2015-05-28 15:46:43 -0700

    app to index

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/WeGetFit");
+mongoose.connect(process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL || "mongodb://localhost/WeGetFit");
 
 
 module.exports.User = require("./user");
