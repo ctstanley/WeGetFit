eed3f3c7cc39b62a7de0016e0c8a9892a2f346b6
ctstanley <ctstanley1@hotmail.com>
2015-06-01 12:29:25 -0700

    test

--- index.js
+++ index.js
@@ -57,6 +57,32 @@ var addPosts = function(eventList) {
 };
 addPosts(events);
 
+var loginHelpers = function (req, res, next) {
+
+  req.login = function (user) {
+    req.session.userId = user._id;
+    req.user = user;
+    return user;
+  };
+
+  req.logout = function () {
+    req.session.userId = null;
+    req.user = null;
+  };
+
+  req.currentUser = function (cb) {
+    var userId = req.session.userId;
+    db.User.
+      findOne({
+        _id: userId
+      }, cb);
+  };
+
+  // careful to have this
+  next(); // real important
+};
+
+app.use("/", loginHelpers)
 
 // Make connection to main page
 app.get("/home", function(req, res) {
@@ -116,7 +142,7 @@ app.post("/login", function(req, res) {
         .authenticate(user.email, user.password,
             function(err, user) {
                 console.log("LOGGING IN!");
-                // req.login(user);
+                req.login(user);
                 res.redirect("/home");
             });
 });
@@ -124,6 +150,10 @@ app.post("/login", function(req, res) {
 app.get("/login", function(req, res) {
     res.sendFile(path.join(views, "Htmls/home.html"));
 
+});
+
+app.post("/logout", function(req, res) {
+  req.logout(user);
 });
 
 app.listen(process.env.PORT || 3000);
